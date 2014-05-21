a1b09fc926c9dbe1d0d27d166a01ca9e81ac990a
katorie <katoriexxxkatorie@gmail.com>
2014-05-22 01:55:06 +0900

    add 05

---
+++ 05/mokuji.rb
@@ -0,0 +1,8 @@
+# 目次を表示するプログラム
+
+width = 30 
+puts "目　次".center(width)
+puts ""
+puts "第1章；　数".ljust(width) + "p.1".rjust(width)
+puts "第2章：　文字".ljust(width) + "p.72".rjust(width)
+puts "第3章：　変数".ljust(width) + "p.118".rjust(width)
